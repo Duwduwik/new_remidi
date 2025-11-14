b8dfa26d9d443452f9923c0e99dda6ac4f30bb2f
shikadiru <dirxz80@gmail.com>
2025-11-15 07:21:30 +0800

    Update
    clear solved

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+// ... route lainnya
 
 Route::get('/home', function () {
     return view('home');    
 });
 
-Route::get('/about', function () {
-    return view('about');    
-});
+
+Route::get('/about', [AboutController::class, 'showAbout'])->name('about');
